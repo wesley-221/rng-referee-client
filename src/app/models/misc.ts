d0a51292721ac6f8460773856aa0f85531b3e3af
Wesley <wesleyalkemade@outlook.com>
2020-04-18 01:33:06 +0200

    Fix not being able to pick maps when there are maps left in the bracket

--- src/app/models/misc.ts
+++ src/app/models/misc.ts
@@ -6,18 +6,16 @@ import { ModBracketMap } from "./osu-mappool/mod-bracket-map";
 export class Misc {
     /**
 	 * Pick a map from a given bracket
+	 * @param multiplayerLobbyService
+	 * @param lobby
 	 * @param bracket the bracket to pick from
+	 * @param ircChannelName
 	 */
 	staticPickRandomMap(multiplayerLobbyService: MultiplayerLobbiesService, lobby: MultiplayerLobby, bracket: ModBracket, ircChannelName: string) {
 		let randomMap: ModBracketMap = null;
-		const lobbyModPicks = lobby.picks.hasOwnProperty(bracket.bracketName) ? lobby.picks[bracket.bracketName] : null;
 
 		do {
 			const map = bracket.beatmaps[Math.floor(Math.random() * bracket.beatmaps.length)];
-
-			if((lobby.teamOneBans.length + lobby.teamTwoBans.length + (lobbyModPicks != null ? lobbyModPicks.length : 0)) == bracket.beatmaps.length) {
-				break;
-			}
 
 			if(!this.staticBeatmapIsBanned(lobby, map.beatmapId) && !this.staticBeatmapIsPicked(lobby, map.beatmapId)) {
 				randomMap = map;
@@ -26,22 +24,22 @@ export class Misc {
 		while(randomMap == null);
 
 		if(randomMap != null) {
-			if(!lobby.picks.hasOwnProperty(bracket.bracketName)) 
+			if(!lobby.picks.hasOwnProperty(bracket.bracketName))
 				lobby.picks[bracket.bracketName] = [];
-
 
 			lobby.picks[bracket.bracketName].push(randomMap.beatmapId);
 			multiplayerLobbyService.update(lobby);
-	
+
 			return randomMap;
 		}
 		else {
 			return null;
 		}
     }
-    
+
     /**
 	 * Check if a beatmap is banned for either of the teams
+	 * @param multiplayerLobby
 	 * @param beatmapId the beatmap to check
 	 */
 	staticBeatmapIsBanned(multiplayerLobby: MultiplayerLobby, beatmapId: number) {
@@ -50,6 +48,7 @@ export class Misc {
 
 	/**
 	 * Check if a beatmap has been picked
+	 * @param mutliplayerLobby
 	 * @param beatmapId the beatmap to check
 	 */
 	staticBeatmapIsPicked(mutliplayerLobby: MultiplayerLobby, beatmapId: number) {
@@ -59,7 +58,7 @@ export class Misc {
 			for(let bracket in mutliplayerLobby.picks) {
 				if(!mutliplayerLobby.picks.hasOwnProperty(bracket))
 					break;
-	
+
 				if(mutliplayerLobby.picks[bracket].indexOf(beatmapId) > -1) {
 					beatmapFound = true;
 					break;
